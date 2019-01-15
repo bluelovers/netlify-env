44fa9ce05299e2bdf902a9a514be6c8f5e9f8fdc
bluelovers <codelovers@users.sourceforge.net>
2019-01-15 12:51:35 +0800

    :bug: Fixing a bug.

--- lib/gitee.ts
+++ lib/gitee.ts
@@ -2,21 +2,13 @@
  * Created by user on 2019/1/15/015.
  */
 
-import pr1 = require('../../../test/temp/pr1.json');
-
-export { pr1 }
-
-export const a = {
-	...pr1
-};
-
 export enum EnumNetlifyHookBodyGiteeAction
 {
 	MERGE = 'merge',
 	CREATE = 'create',
 }
 
-export enum EnumNetlifyHookBodyGiteePullRequest
+export enum EnumNetlifyHookBodyGiteePullRequestStatus
 {
 	MERGED = 'merged',
 	OPEN = 'open',
@@ -57,13 +49,66 @@ export interface INetlifyHookBodyGiteeWhoLite
 	"url": string;
 }
 
+export interface INetlifyHookBodyGiteeRepo
+{
+	"project": INetlifyHookBodyGiteeRepoItem;
+	"repository": INetlifyHookBodyGiteeRepoItem;
+}
+
+export interface INetlifyHookBodyGiteeRepoItem
+{
+	"id": number;
+	"name": string;
+	"path": string;
+	"full_name": string;
+	"owner": INetlifyHookBodyGiteeWho;
+	"private": boolean;
+	"html_url": string;
+	"url": string;
+	"description": string;
+	"fork": boolean;
+	"created_at": string;
+	"updated_at": string;
+	"pushed_at": string;
+	"git_url": string;
+	"ssh_url": string;
+	"clone_url": string;
+	"svn_url": string;
+	"git_http_url": string;
+	"git_ssh_url": string;
+	"git_svn_url": string;
+	"homepage": string;
+	"stargazers_count": number;
+	"watchers_count": number;
+	"forks_count": number;
+	"language": any;
+	"has_issues": boolean;
+	"has_wiki": boolean;
+	"has_pages": boolean;
+	"license": any;
+	"open_issues_count": number;
+	"default_branch": string;
+	"namespace": string;
+	"name_with_namespace": string;
+	"path_with_namespace": string;
+}
+
+export interface INetlifyHookBodyGiteeHeadBase
+{
+	"label": string;
+	"ref": string;
+	"sha": string;
+	"user": INetlifyHookBodyGiteeWho;
+	"repo": INetlifyHookBodyGiteeRepoItem;
+}
+
 export interface INetlifyHookBodyGitee
 {
 	"action": EnumNetlifyHookBodyGiteeAction | string;
 	"pull_request": {
 		"id": number;
 		"number": number;
-		"state": EnumNetlifyHookBodyGiteePullRequest | string;
+		"state": EnumNetlifyHookBodyGiteePullRequestStatus | string;
 		"html_url": string;
 		"diff_url": string;
 		"patch_url": string;
@@ -83,90 +128,8 @@ export interface INetlifyHookBodyGitee
 		"need_test": boolean;
 		"need_review": boolean;
 		"milestone": any;
-		"head": {
-			"label": string;
-			"ref": string;
-			"sha": string;
-			"user": INetlifyHookBodyGiteeWho;
-			"repo": {
-				"id": number;
-				"name": string;
-				"path": string;
-				"full_name": string;
-				"owner": INetlifyHookBodyGiteeWho;
-				"private": boolean;
-				"html_url": string;
-				"url": string;
-				"description": string;
-				"fork": boolean;
-				"created_at": string;
-				"updated_at": string;
-				"pushed_at": string;
-				"git_url": string;
-				"ssh_url": string;
-				"clone_url": string;
-				"svn_url": string;
-				"git_http_url": string;
-				"git_ssh_url": string;
-				"git_svn_url": string;
-				"homepage": string;
-				"stargazers_count": number;
-				"watchers_count": number;
-				"forks_count": number;
-				"language": any;
-				"has_issues": boolean;
-				"has_wiki": boolean;
-				"has_pages": boolean;
-				"license": any;
-				"open_issues_count": number;
-				"default_branch": string;
-				"namespace": string;
-				"name_with_namespace": string;
-				"path_with_namespace": string;
-			};
-		};
-		"base": {
-			"label": string;
-			"ref": string;
-			"sha": string;
-			"user": INetlifyHookBodyGiteeWho;
-			"repo": {
-				"id": number;
-				"name": string;
-				"path": string;
-				"full_name": string;
-				"owner": INetlifyHookBodyGiteeWho;
-				"private": boolean;
-				"html_url": string;
-				"url": string;
-				"description": string;
-				"fork": boolean;
-				"created_at": string;
-				"updated_at": string;
-				"pushed_at": string;
-				"git_url": string;
-				"ssh_url": string;
-				"clone_url": string;
-				"svn_url": string;
-				"git_http_url": string;
-				"git_ssh_url": string;
-				"git_svn_url": string;
-				"homepage": string;
-				"stargazers_count": number;
-				"watchers_count": number;
-				"forks_count": number;
-				"language": any;
-				"has_issues": boolean;
-				"has_wiki": boolean;
-				"has_pages": boolean;
-				"license": any;
-				"open_issues_count": number;
-				"default_branch": string;
-				"namespace": string;
-				"name_with_namespace": string;
-				"path_with_namespace": string;
-			};
-		};
+		"head": INetlifyHookBodyGiteeHeadBase;
+		"base": INetlifyHookBodyGiteeHeadBase;
 		"merged": boolean;
 		"mergeable": boolean;
 		"merge_status": string;
@@ -181,261 +144,21 @@ export interface INetlifyHookBodyGitee
 	"iid": number;
 	"title": string;
 	"body": string;
-	"state": EnumNetlifyHookBodyGiteePullRequest | string;
+	"state": EnumNetlifyHookBodyGiteePullRequestStatus | string;
 	"merge_status": EnumNetlifyHookBodyGiteeMergeStatus | string;
 	"merge_commit_sha": string;
 	"url": string;
 	"source_branch": string;
-	"source_repo": {
-		"project": {
-			"id": number;
-			"name": string;
-			"path": string;
-			"full_name": string;
-			"owner": INetlifyHookBodyGiteeWho;
-			"private": boolean;
-			"html_url": string;
-			"url": string;
-			"description": string;
-			"fork": boolean;
-			"created_at": string;
-			"updated_at": string;
-			"pushed_at": string;
-			"git_url": string;
-			"ssh_url": string;
-			"clone_url": string;
-			"svn_url": string;
-			"git_http_url": string;
-			"git_ssh_url": string;
-			"git_svn_url": string;
-			"homepage": string;
-			"stargazers_count": number;
-			"watchers_count": number;
-			"forks_count": number;
-			"language": any;
-			"has_issues": boolean;
-			"has_wiki": boolean;
-			"has_pages": boolean;
-			"license": any;
-			"open_issues_count": number;
-			"default_branch": string;
-			"namespace": string;
-			"name_with_namespace": string;
-			"path_with_namespace": string;
-		};
-		"repository": {
-			"id": number;
-			"name": string;
-			"path": string;
-			"full_name": string;
-			"owner": {
-				"id": number;
-				"name": string;
-				"email": string;
-				"username": string;
-				"user_name": string;
-				"url": string;
-				"login": string;
-				"avatar_url": string;
-				"html_url": string;
-				"type": string;
-				"site_admin": boolean;
-			};
-			"private": boolean;
-			"html_url": string;
-			"url": string;
-			"description": string;
-			"fork": boolean;
-			"created_at": string;
-			"updated_at": string;
-			"pushed_at": string;
-			"git_url": string;
-			"ssh_url": string;
-			"clone_url": string;
-			"svn_url": string;
-			"git_http_url": string;
-			"git_ssh_url": string;
-			"git_svn_url": string;
-			"homepage": string;
-			"stargazers_count": number;
-			"watchers_count": number;
-			"forks_count": number;
-			"language": any;
-			"has_issues": boolean;
-			"has_wiki": boolean;
-			"has_pages": boolean;
-			"license": any;
-			"open_issues_count": number;
-			"default_branch": string;
-			"namespace": string;
-			"name_with_namespace": string;
-			"path_with_namespace": string;
-		};
-	};
+	"source_repo": INetlifyHookBodyGiteeRepo;
 	"target_branch": string;
-	"target_repo": {
-		"project": {
-			"id": number;
-			"name": string;
-			"path": string;
-			"full_name": string;
-			"owner": INetlifyHookBodyGiteeWho;
-			"private": boolean;
-			"html_url": string;
-			"url": string;
-			"description": string;
-			"fork": boolean;
-			"created_at": string;
-			"updated_at": string;
-			"pushed_at": string;
-			"git_url": string;
-			"ssh_url": string;
-			"clone_url": string;
-			"svn_url": string;
-			"git_http_url": string;
-			"git_ssh_url": string;
-			"git_svn_url": string;
-			"homepage": string;
-			"stargazers_count": number;
-			"watchers_count": number;
-			"forks_count": number;
-			"language": any;
-			"has_issues": boolean;
-			"has_wiki": boolean;
-			"has_pages": boolean;
-			"license": any;
-			"open_issues_count": number;
-			"default_branch": string;
-			"namespace": string;
-			"name_with_namespace": string;
-			"path_with_namespace": string;
-		};
-		"repository": {
-			"id": number;
-			"name": string;
-			"path": string;
-			"full_name": string;
-			"owner": {
-				"id": number;
-				"name": string;
-				"email": string;
-				"username": string;
-				"user_name": string;
-				"url": string;
-				"login": string;
-				"avatar_url": string;
-				"html_url": string;
-				"type": string;
-				"site_admin": boolean;
-			};
-			"private": boolean;
-			"html_url": string;
-			"url": string;
-			"description": string;
-			"fork": boolean;
-			"created_at": string;
-			"updated_at": string;
-			"pushed_at": string;
-			"git_url": string;
-			"ssh_url": string;
-			"clone_url": string;
-			"svn_url": string;
-			"git_http_url": string;
-			"git_ssh_url": string;
-			"git_svn_url": string;
-			"homepage": string;
-			"stargazers_count": number;
-			"watchers_count": number;
-			"forks_count": number;
-			"language": any;
-			"has_issues": boolean;
-			"has_wiki": boolean;
-			"has_pages": boolean;
-			"license": any;
-			"open_issues_count": number;
-			"default_branch": string;
-			"namespace": string;
-			"name_with_namespace": string;
-			"path_with_namespace": string;
-		};
-	};
-	"project": {
-		"id": number;
-		"name": string;
-		"path": string;
-		"full_name": string;
-		"owner": INetlifyHookBodyGiteeWho;
-		"private": boolean;
-		"html_url": string;
-		"url": string;
-		"description": string;
-		"fork": boolean;
-		"created_at": string;
-		"updated_at": string;
-		"pushed_at": string;
-		"git_url": string;
-		"ssh_url": string;
-		"clone_url": string;
-		"svn_url": string;
-		"git_http_url": string;
-		"git_ssh_url": string;
-		"git_svn_url": string;
-		"homepage": string;
-		"stargazers_count": number;
-		"watchers_count": number;
-		"forks_count": number;
-		"language": any;
-		"has_issues": boolean;
-		"has_wiki": boolean;
-		"has_pages": boolean;
-		"license": any;
-		"open_issues_count": number;
-		"default_branch": string;
-		"namespace": string;
-		"name_with_namespace": string;
-		"path_with_namespace": string;
-	};
-	"repository": {
-		"id": number;
-		"name": string;
-		"path": string;
-		"full_name": string;
-		"owner": INetlifyHookBodyGiteeWho;
-		"private": boolean;
-		"html_url": string;
-		"url": string;
-		"description": string;
-		"fork": boolean;
-		"created_at": string;
-		"updated_at": string;
-		"pushed_at": string;
-		"git_url": string;
-		"ssh_url": string;
-		"clone_url": string;
-		"svn_url": string;
-		"git_http_url": string;
-		"git_ssh_url": string;
-		"git_svn_url": string;
-		"homepage": string;
-		"stargazers_count": number;
-		"watchers_count": number;
-		"forks_count": number;
-		"language": any;
-		"has_issues": boolean;
-		"has_wiki": boolean;
-		"has_pages": boolean;
-		"license": any;
-		"open_issues_count": number;
-		"default_branch": string;
-		"namespace": string;
-		"name_with_namespace": string;
-		"path_with_namespace": string;
-	};
+	"target_repo": INetlifyHookBodyGiteeRepo;
+	"project": INetlifyHookBodyGiteeRepoItem;
+	"repository": INetlifyHookBodyGiteeRepoItem;
 	"author": INetlifyHookBodyGiteeWho;
 	"updated_by": INetlifyHookBodyGiteeWho;
 	"sender": INetlifyHookBodyGiteeWho;
 	"target_user"?: INetlifyHookBodyGiteeWhoLite;
 	"enterprise": any;
-	"hook_name": string;
+	"hook_name": EnumNetlifyHookBodyGiteeHookName | string;
 	"password": string;
 }
